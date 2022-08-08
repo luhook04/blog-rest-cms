94a13105167f422960fecbd57d4cc47e69bc4fee
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-08 19:30:27 -0400

    Create an initialState and use the authReducer on it
    On App component mount, retrieve token from local storage and call the
    dispatch function on it. Also, pass the retrieved token as a payload.
    Wrap app with the created context and pass the state and dispatch
    through to it.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { createContext, useEffect, useReducer } from "react";
 import "./App.css";
+import authReducer from "./authReducer";
 
-function App() {
-  return <div className="App"></div>;
-}
+const AuthContext = createContext();
+
+const App = () => {
+  const initialState = {
+    isAuthenticated: false,
+    token: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    token && dispatch({ type: "login", payload: { token: token } });
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ state, dispatch }}
+    ></AuthContext.Provider>
+  );
+};
 
 export default App;
