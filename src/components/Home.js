6873c2225c06ddee15f09c3cd3c027fc2fded15f
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-09 16:20:42 -0400

    Add Routes to the home page for the user actions
    Create routes for the PostList, EditPost, and NewPost components.
    Conditionally render either the intended component or the login
    component based off of user authentication from the AuthContext.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,33 @@
 import React, { useContext } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { AuthContext } from "../App";
+import EditPost from "./EditPost";
+import NewPost from "./NewPost";
+import PostList from "./PostList";
+import Login from "./Login";
 
 const Home = () => {
   const { state } = useContext(AuthContext);
 
   return (
     <BrowserRouter basename="/">
-      <Routes></Routes>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={state.isAuthenticated ? <PostList /> : <Login />}
+        />
+        <Route
+          exact
+          path="/posts/:postId"
+          element={state.isAuthenticated ? <EditPost /> : <Login />}
+        />
+        <Route
+          exact
+          path="/newpost"
+          element={state.isAuthenticated ? <NewPost /> : <Login />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
