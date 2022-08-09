153493ecf50f1aa5b7838ca60d4c16451351b5bd
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-09 16:46:13 -0400

    Create the html for the Login component
    Pass the newly created state to the corresponding input as it's value.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 const Login = () => {
-  return <h3>Login Component</h3>;
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={user.username}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          value={user.password}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
