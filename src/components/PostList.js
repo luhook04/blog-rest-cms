c54846d6ab0ab9f6017e1d49e9700c60902903ae
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-09 20:53:34 -0400

    Fetch all posts on PostList component render and pass req data as state

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const PostList = () => {
-  return <h3>PostList Component</h3>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const req = await fetch(
+          "https://dry-hamlet-86450.herokuapp.com/api/posts"
+        );
+        if (req.status !== 200) {
+          return;
+        }
+        const reqJson = await req.json();
+        setPosts(reqJson.posts);
+      } catch (err) {}
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts ? (
+        <div>
+          {posts.map((post) => {
+            return <p key={post._id}>{post.title}</p>;
+          })}
+        </div>
+      ) : (
+        <h2>No Posts</h2>
+      )}
+    </div>
+  );
 };
 
 export default PostList;
