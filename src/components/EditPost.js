99a74556db94cf41a04652d1b31fbe6f23934c5e
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-10 15:29:12 -0400

    Fix uncontrolled input warning by only passing necessary post values

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const EditPost = () => {
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState({ title: "", text: "" });
   const [comments, setComments] = useState([]);
 
   const { postId } = useParams();
@@ -17,7 +17,9 @@ const EditPost = () => {
           return;
         }
         const reqJson = await req.json();
-        setPost(reqJson.post);
+        const title = reqJson.post.title;
+        const text = reqJson.post.text;
+        setPost({ title, text });
       } catch (err) {}
     };
     getPost();
@@ -37,16 +39,35 @@ const EditPost = () => {
     getComments();
   }, [postId]);
 
+  const handleEdit = (e) => {
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
       <form>
         <div>
           <label htmlFor="title">Title: </label>
-          <input type="text" name="title" id="title" value={post.title} />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={(e) => handleEdit(e)}
+            value={post.title}
+          />
         </div>
         <div>
           <label htmlFor="text">Edit Text: </label>
-          <textarea type="text" name="text" id="text" value={post.text} />
+          <textarea
+            type="text"
+            name="text"
+            id="text"
+            onChange={(e) => handleEdit(e)}
+            value={post.text}
+          />
         </div>
         <button type="submit">Submit Edit</button>
       </form>
