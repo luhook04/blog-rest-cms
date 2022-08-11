2adc3604889a53aa8364db43afb67329a0488465
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-11 13:11:19 -0400

    Add the html for the NewPost component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   const { dispatch } = useContext(AuthContext);
+
   const logOut = () => {
     dispatch({ type: "logout" });
   };
--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../App";
 
 const NewPost = () => {
-  return <h3>New Post Component</h3>;
+  return (
+    <div className="new-post-container">
+      <form>
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input type="text" id="title" name="title" />
+        </div>
+        <div>
+          <label htmlFor="text">Post: </label>
+          <input type="text" id="text" name="text" />
+        </div>
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
 };
 
 export default NewPost;
