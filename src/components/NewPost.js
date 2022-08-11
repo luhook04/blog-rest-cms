da2864eca7a0aaf89b1f30dedf8afa0051c9b12e
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-11 13:16:40 -0400

    Create handleChange function
    Add handleChange function to be called onChange for each input. Make the
    value of each input be the corresponding state attribute.

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -2,16 +2,38 @@ import React, { useState, useContext } from "react";
 import { AuthContext } from "../App";
 
 const NewPost = () => {
+  const [newPost, setNewPost] = useState({ title: "", text: "" });
+  const { state } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setNewPost({
+      ...newPost,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="new-post-container">
       <form>
         <div>
           <label htmlFor="title">Title: </label>
-          <input type="text" id="title" name="title" />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            onChange={(e) => handleChange(e)}
+            value={newPost.title}
+          />
         </div>
         <div>
           <label htmlFor="text">Post: </label>
-          <input type="text" id="text" name="text" />
+          <input
+            type="text"
+            id="text"
+            name="text"
+            onChange={(e) => handleChange(e)}
+            value={newPost.text}
+          />
         </div>
         <button type="submit">Post</button>
       </form>
