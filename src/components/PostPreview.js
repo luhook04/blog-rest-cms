b7ef3b9f23e0c0384ce5ceaa0d1f8c56cc8ecf18
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-11 14:36:24 -0400

    Show the published status of each post in the PostPreview component

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 import CommentIcon from "@mui/icons-material/Comment";
 
 const PostPreview = ({ post, deletePost }) => {
+  console.log(post);
   const commentCount = post.comments.length;
   return (
     <div className="post-preview">
       <h3 className="post-title">{post.title}</h3>
       <h4 className="post-author">By: {post.authorName}</h4>
       <p className="post-date-preview">{post.date}</p>
+      <p className="publish">
+        Published: {String(post.published).toUpperCase()}
+      </p>
       <div className="view-post-container">
         <Link to={`/posts/${post._id}`}>
           <button type="button">Edit Post</button>
